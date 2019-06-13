4a7f4665c8b6e17d73107baa0159f612b5046c66
Hoang Ngoc <hoangngoc444444@gmail.com>
2019-06-13 16:26:15 +0700

    xoá hết

--- app/QHOnline/helpers.php
+++
@@ -1,9 +0,0 @@
-<?php
-use App\QHOnline\Facades\Tool;
-if (!function_exists('get_thumbnail')) {
-    function get_thumbnail($filename, $suffix = '_thumb') {
-        return Tool::getThumbnail($filename, $suffix);
-    }
-}
-
-
